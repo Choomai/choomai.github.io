f7953ac58900b0cbd129bd3a5cb3ef1ba91ee999
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-08-30 21:29:51 +0700

    Add basic stuff....lmao

---
+++ gettitle.js
@@ -0,0 +1,6 @@
+let domain = window.location.hostname;
+let path = window.location.pathname;
+path = path.replaceAll("%20"," ");
+const tabtt = document.getElementById("tab-title");
+const pagett = document.getElementById("page-title");
+// const back = document.getElementById("goback");
