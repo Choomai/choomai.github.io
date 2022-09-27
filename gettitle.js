6966916076a3220d64045ee28bb2c74f87941a75
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-09-27 19:51:55 +0700

    Add... a lot of stuff :)

--- gettitle.js
+++ gettitle.js
@@ -1,6 +1,63 @@
-let domain = window.location.hostname;
+let domain = null;
 let path = window.location.pathname;
-path = path.replaceAll("%20"," ");
+path = decodeURI(path);
 const tabtt = document.getElementById("tab-title");
 const pagett = document.getElementById("page-title");
-// const back = document.getElementById("goback");
+function isEmpty(inp) {
+    if (inp == "") {return true} else {return false}
+};
+function addTitle(inp) {
+    switch (inp) {
+        case 0:
+            domain = window.location.hostname;
+            // If HTTP error, return Client / Server Error title
+            if (typeof errCode == "number") {
+                if (errCode >= 500) {
+                    pagett.innerHTML = domain + " - Server Error";
+                } else if (errCode >= 400) {
+                    pagett.innerHTML = domain + " - Client Error";
+                };
+            };
+            if (path.substring(0,3) == "/dl") {
+                domain = "dl." + window.location.hostname;
+                path = path.substring(3,path.length);
+            };
+            if (isEmpty(tabtt.textContent)) {
+                tabtt.innerHTML = path + " - " + domain;
+            };
+            if (isEmpty(pagett.textContent)) {
+                pagett.innerHTML = domain + " - " + path;
+            };
+            break;
+        case 1:
+            domain = document.createElement("a");
+            domain.setAttribute("href","/");
+            domain.setAttribute("class","title-txt")
+            domain.textContent = window.location.hostname;
+            // If HTTP error, return Client / Server Error title
+            if (typeof errCode == "number") {
+                if (errCode >= 500) {
+                    pagett.innerHTML = " - Server Error";
+                    pagett.prepend(domain);
+                } else if (errCode >= 400) {
+                    pagett.innerHTML = " - Client Error";
+                    pagett.prepend(domain);
+                };
+            };
+            if (isEmpty(tabtt.textContent)) {
+                tabtt.innerHTML = path + " - " + domain.textContent;
+            };
+            if (isEmpty(pagett.textContent)) {
+                pagett.innerHTML = " - " + path;
+                pagett.prepend(domain);
+            };
+            break;
+    }
+};
+// If the page is dl, return no-hyperlink
+if (path.substring(0,3) == "/dl") {addTitle(0);} else {addTitle(1)};
+// function willBeRemoved() {
+//     if (confirm("I'm currently replacing \"setTimeout()\" with \"Async/Await\".\nReturn later or press \"OK\" to ignore this warning.")) {
+//         window.location.href = "/dl";
+//     };
+// };
---
+++ userdebug/policy.js
@@ -0,0 +1,111 @@
+/* 
+    Using RANDOM.ORG API to generate an integer (range: 1 to 3)
+    Key Name: "choomai-web", License: Developer
+    Daily limits: 1000 reqs/day & 250000 bits/day, Rate limit: 10 reqs/second
+*/
+let ranInt = NaN;
+let totalClick = 0;
+let hasClicked = false;
+let vid = null;
+function vidbase() {
+    document.getElementById("txt").innerHTML = '<b>WARN: Scripts may close only the windows that were opened by them.<b>.<br><video id="vid" class="fullscreen" loop></video>';
+    vid = document.getElementById("vid");
+};
+function vid_reqfs(vid_inp) {
+    hasClicked = true;
+    window.onbeforeunload = function() {
+        if (hasClicked) return true;
+    };
+    vid.src = vid_inp;
+    vid.requestFullscreen();
+    vid.play();
+};
+function closetab() {
+    if (!document.webkitIsFullScreen) {
+        window.close();
+    }
+};
+function redirect() {
+    if(document.getElementById("continue").checked) {
+        // alert("This video is marked as dangerous (or hidden by some weird mf [not me ofc] 😏), and I don't want to go to jail so... 😅")
+        vidbase();
+        vid_reqfs("userdebug.mp4");
+        vid.style.objectFit = "cover";
+    }
+    else if(document.getElementById("vidoverlay").checked) {
+        // Try your luck here 😂
+        document.getElementById("txt").innerHTML = "Wait...";
+        fetch("https://api.random.org/json-rpc/4/invoke", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "jsonrpc": "2.0",
+                "method": "generateIntegers",
+                "id": 0,
+                "params": {
+                    "apiKey": "913f26a3-6209-4ca4-b45a-aeac662594ce",
+                    "n": 1,
+                    "min": 1,
+                    "max": 5
+                }
+            })
+        })
+        .then(response => {return response.json();})
+        .then(response => {
+            random = (response.result);
+            ranInt = response.result.random.data[0];
+            console.log("Request left: "+random.requestsLeft);
+            console.log("Bits left: "+random.bitsLeft);
+            vidbase();
+            switch(ranInt) {
+                case 1: vid_reqfs("chuitheconcac.mp4");
+                break;
+                case 2: vid_reqfs("frifai.mp4");
+                break;
+                case 3: vid_reqfs("fnaf%20jumpscare.mp4");
+                break;
+                case 4: vid_reqfs("rickroll.mp4");
+                break;
+                case 5: vid_reqfs("jumpscare.mp4");
+                break;
+                case NaN: alert("Can't generate random integer from \"RANDOM.ORG\".")
+                break;
+            }
+            if (document.addEventListener) {document.addEventListener('fullscreenchange', closetab, false);}
+        });
+    }
+};
+function redirectV2() {
+    // Is this an easter egg ? 😂
+    totalClick++;
+    if (totalClick <= 3) {alert("This function has been disabled by the owner of this webiste.")}
+    else if (totalClick > 3 && totalClick <= 5) {alert("Thi? f?nct?on has been d?sabled by the own?r o? this webi?te.")}
+    else if (totalClick > 5 && totalClick <= 10) {alert("T?i? f?nct\\on ha? be?? d$sabled ?y t?? own?r o? t?!? w?bi?te.")}
+    else if (totalClick > 10 && totalClick <= 12) {alert("T¾i¾ f@n¾t¾o& ha½ ?e?? d$?a?¾?? ?y t?? o!n¼r o? t\\½? w?b?½te.")}
+    else if (totalClick == 12) {alert("Press OK to ignore the waring.");redirect()}
+    else if (totalClick > 13) {redirect()};
+};
+// For countdown
+let endDate = new Date("Sep 27, 2022 20:00:00").getTime();
+const timerDay = document.getElementById("timer-days");
+const timerHour = document.getElementById("timer-hours");
+const timerMin = document.getElementById("timer-mins");
+const timerSec = document.getElementById("timer-secs");
+setInterval(() => {
+    let now = new Date().getTime();
+    let t = endDate - now;
+    if (t >= 0) {
+        // let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+        // timerDay.innerHTML = days + "<span class='label'>DAY(S)</span>";
+        timerHour.innerHTML = hours + "<span class='label'>h</span>";
+        timerMin.innerHTML = mins + "<span class='label'>m</span>";
+        timerSec.innerHTML = secs + "<span class='label'>s</span>";
+    } else {
+        document.getElementById("timer").innerHTML = "The countdown is over!";
+    }
+}, 1000);
