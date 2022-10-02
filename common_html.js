9b2059c9334f1c027e22666f3ee56026f8bb3732
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-10-02 11:38:07 +0700

    None

---
+++ common_html.js
@@ -0,0 +1,111 @@
+// Prep.
+let domain = null;
+let path = window.location.pathname;
+path = decodeURI(path);
+let slashMap = [];
+for (let i = 0; i < path.length; i++) {
+    if (path[i] == "/") {
+        slashMap.push(i);
+    };
+};
+let slashCount = slashMap.length;
+let currentDir = null;
+if (path.substring(slashMap[slashCount-2]+1,slashMap[slashCount-1]) == "dl") {
+    currentDir = "/";
+} else {
+    currentDir = path.substring(slashMap[slashCount-2]+1,slashMap[slashCount-1]);
+}
+const tabtt = document.getElementById("tab-title");
+const pagett = document.getElementById("page-title");
+let contact = document.getElementById("contact");
+// End prep.
+function addContact(inp) {
+    let social = null;
+    let socialImg = null;
+    let email = document.createElement("h2")
+    email.setAttribute("class","center email");
+    if (inp == 1) {email.textContent = "Report bugs & corrupted files: "} else {email.textContent = "Email: "};
+    let emailURL = document.createElement("a")
+    emailURL.href = "mailto:nguyencaonguyen0944322545@gmail.com";
+    emailURL.style = "color:#4287F5";
+    emailURL.textContent = "nguyencaonguyen0944322545@gmail.com";
+    email.appendChild(emailURL);
+    contact.appendChild(email);
+    for (let i = 0; i <= 3; i++) {
+        const urlMap = [
+            "https://facebook.com/uranidiot0606",
+            "https://twitter.com/choomai0",
+            "https://github.com/Choomai",
+            "https://discord.gg/J8tsBMUfUY"
+        ];
+        const imgMap = [
+            "https://cdn-icons-png.flaticon.com/512/5968/5968764.png",
+            "https://cdn-icons-png.flaticon.com/128/733/733579.png",
+            "https://github.githubassets.com/favicons/favicon-dark.svg",
+            "https://cdn-icons-png.flaticon.com/512/5968/5968756.png"
+        ];
+        social = document.createElement("a")
+        social.href = urlMap[i];
+        socialImg = document.createElement("img")
+        socialImg.setAttribute("class","profile")
+        socialImg.src = imgMap[i];
+        social.appendChild(socialImg);
+        contact.appendChild(social);
+    }
+};
+function isEmpty(inp) {
+    if (inp == "") {return true} else {return false}
+};
+function addTitle(inp) {
+    switch (inp) {
+        case 0:
+            domain = window.location.hostname;
+            // If HTTP error, return Client / Server Error title
+            if (typeof errCode == "number") {
+                if (errCode >= 500) {
+                    pagett.innerHTML = domain + " - Server Error";
+                } else if (errCode >= 400) {
+                    pagett.innerHTML = domain + " - Client Error";
+                };
+            };
+            // Convert /dl/ to dl.*
+            if (path.substring(0,3) == "/dl") {
+                domain = "dl." + window.location.hostname;
+                path = path.substring(3,path.length);
+            };
+            if (isEmpty(tabtt.textContent)) {
+                // tabtt.innerHTML = path + " - " + domain;
+                tabtt.innerHTML = currentDir + " - Server Repository";
+            };
+            if (isEmpty(pagett.textContent)) {
+                pagett.innerHTML = domain + " - " + path;
+            };
+            break;
+        case 1:
+            domain = document.createElement("a");
+            domain.setAttribute("href","/");
+            domain.setAttribute("class","title-txt")
+            domain.textContent = window.location.hostname;
+            // If HTTP error, return Client / Server Error title
+            if (typeof errCode == "number") {
+                if (errCode >= 500) {
+                    pagett.innerHTML = " - Server Error";
+                    pagett.prepend(domain);
+                } else if (errCode >= 400) {
+                    pagett.innerHTML = " - Client Error";
+                    pagett.prepend(domain);
+                };
+            };
+            if (isEmpty(tabtt.textContent)) {
+                // tabtt.innerHTML = path + " - " + domain.textContent;
+                tabtt.innerHTML = path.substring(slashMap[slashCount-2]+1,slashMap[slashCount-1]) + " - Server Repository";
+            };
+            if (isEmpty(pagett.textContent)) {
+                pagett.innerHTML = " - " + path;
+                pagett.prepend(domain);
+            };
+            break;
+    }
+};
+// If current page is /dl/*, return no-hyperlink title & "Report bugs & corrupted files: "
+if (path.substring(0,3) == "/dl") {addTitle(0);addContact(1);} else {addTitle(1);addContact(0)};
--- error/return_err.js
+++ error/return_err.js
@@ -2,29 +2,18 @@ let httpCode = null;
 let info0 = null;
 let info1 = null;
 let domain = window.location.hostname;
-fetch("/error/errorinfo.json")
-.then(response => {
-    return response.json();
-})
-.then(data => httpCode = (data))
-.then(() => {
-    let pagett = document.getElementById("page-title");
-    let tabtt = document.getElementById("tab-title");
-    info0 = document.getElementById("info_0");
-    info1 = document.getElementById("info_1");
-    // fullErrCode = path.slice(path.lastIndexOf("/")+1,-5).replace("-",".");
-    // errCode = parseInt(fullErrCode.substring(0,3));
-    // if (fullErrCode.length >= 5) {
-    //     subStat = fullErrCode.substring(fullErrCode.indexOf(".")+1);
-    // };
-    currentInfo = httpCode.find(info => info.code == errCode);
-    if (errCode >= 500) {
-        pagett.innerHTML = domain + " - Server Error";
-    } else {
-        pagett.innerHTML = domain + " - Client Error";
-    }
-    tabtt.innerHTML = currentInfo.err[subStat].info_0;
-    document.getElementById("wait_notify").remove();
-    info0.innerHTML = currentInfo.err[subStat].info_0;
-    info1.innerHTML = currentInfo.err[subStat].info_1;
-})
+let pagett = document.getElementById("page-title");
+let tabtt = document.getElementById("tab-title");
+main_info = "404 - File or directory not found.";
+sub_info = "The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.";
+info0 = document.getElementById("info_0");
+info1 = document.getElementById("info_1");
+// fullErrCode = path.slice(path.lastIndexOf("/")+1,-5).replace("-",".");
+// errCode = parseInt(fullErrCode.substring(0,3));
+// if (fullErrCode.length >= 5) {
+//     subStat = fullErrCode.substring(fullErrCode.indexOf(".")+1);
+// };
+pagett.innerHTML = domain + " - Client Error";
+tabtt.innerHTML = main_info
+info0.innerHTML = main_info;
+info1.innerHTML = sub_info;
