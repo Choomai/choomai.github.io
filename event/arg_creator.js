e0c558b6ad1b9f18b7faaea9c44d5c8b23a26468
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-11-18 22:13:39 +0700

    Better UI and add size arg

--- event/arg_creator.js
+++ event/arg_creator.js
@@ -1,22 +1,21 @@
+let output = document.getElementById("arg-output");
 function createArg() {
-    const output = document.getElementById("arg-output");
-    const username = document.getElementById("username").value;
-    const color = document.getElementById("color").value;
-    const rotate = document.getElementById("rotate").value;
-    const wiggle = document.getElementById("wiggle").value;
-    if (isHex.test(color) || isHexWithHashtag.test(knownColor[color]) || color === "") {
-        let hyperlink = document.createElement("a");
-        hyperlink.classList.add("arg-output");
-        hyperlink.target = "_blank";
-        hyperlink.href = location.href + "heart/";
-        if (username) {hyperlink.href = hyperlink.href + "?name=" + encodeURI(username)};
-        if (color) {hyperlink.href = hyperlink.href + "&color=" + encodeURI(color)};
-        if (rotate === "false") {hyperlink.href = hyperlink.href + "&rotate=" + rotate};
-        if (wiggle === "false") {hyperlink.href = hyperlink.href + "&wiggle=" + wiggle};
-        hyperlink.innerHTML = hyperlink.href;
-        output.innerHTML = "";
-        output.appendChild(hyperlink);
+    let username = document.getElementById("username").value;
+    let color = document.getElementById("color").value;
+    let size = document.getElementById("size").value;
+    let rotate = document.getElementById("rotate").value;
+    let wiggle = document.getElementById("wiggle").value;
+    if (isHex.test(color) || isHexWithHashtag.test(knownColor[color]) || color === "" || color == "rgb") {
+        output.classList.add("arg-output");
+        output.target = "_blank";
+        output.href = location.href + "heart/";
+        if (username) {output.href = output.href + "?name=" + encodeURI(username)};
+        if (size) {output.href = output.href + "&size=" + size};
+        if (color) {output.href = output.href + "&color=" + encodeURI(color)};
+        if (rotate === "false") {output.href = output.href + "&rotate=" + rotate};
+        if (wiggle === "false") {output.href = output.href + "&wiggle=" + wiggle};
+        output.innerHTML = output.href;
     } else {
         output.innerHTML = "Wrong format!";
-    }
+    };
 };
