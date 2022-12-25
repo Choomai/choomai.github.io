ce095cc0f3006c0ecf6cdb5f22cbdc60af33a8e8
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-12-25 14:01:50 +0700

    error page CDN for main

---
+++ error/README.md
@@ -0,0 +1,5 @@
+This folder contains files for delivering error pages.
+
+- `JSON` to return error info. 
+- `error.css`
+- `return_err.js` fetch `JSON` file and put it on container.
---
+++ error/return_err.js
@@ -0,0 +1,19 @@
+let httpCode = null;
+let info0 = null;
+let info1 = null;
+fetch("/error/errinfo.json")
+.then(response => {
+    return response.json();
+})
+.then(data => httpCode = (data))
+.then(() => {
+    info0 = document.getElementById("info_0");
+    info1 = document.getElementById("info_1");
+    currentInfo = httpCode.find(info => info.code == errCode);
+    addTitle(1);
+    document.title = currentInfo.err[subStat].info_0;
+    document.getElementById("wait_notify").remove();
+    info0.innerHTML = currentInfo.err[subStat].info_0;
+    info1.innerHTML = currentInfo.err[subStat].info_1;
+    document.getElementById("service").innerHTML = "Powered by IIS v10.0.19041.1 / Windows 10 Pro 22H2";
+});
