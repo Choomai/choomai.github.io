f748e06fe593fb5529df18e6771ad2ec5a9a2e52
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-01-23 23:45:28 +0700

    Some changes...

--- error/return_err.js
+++ error/return_err.js
@@ -1,19 +1,9 @@
-let httpCode = null;
-let info0 = null;
-let info1 = null;
-fetch("https://cdn.jsdelivr.net/gh/Choomai/choomai.github.io/error/errinfo.min.json")
-.then(response => {
-    return response.json();
-})
-.then(data => httpCode = (data))
-.then(() => {
-    info0 = document.getElementById("info_0");
-    info1 = document.getElementById("info_1");
-    currentInfo = httpCode.find(info => info.code == errCode);
-    addTitle(1);
-    document.title = currentInfo.err[subStat].info_0;
-    document.getElementById("wait_notify").remove();
-    info0.innerHTML = currentInfo.err[subStat].info_0;
-    info1.innerHTML = currentInfo.err[subStat].info_1;
-    document.getElementById("service").innerHTML = "Powered by IIS v10.0.19041.1 / Windows 10 Pro 22H2";
-});
+let info0 = document.getElementById("info_0");
+let info1 = document.getElementById("info_1");
+currentInfo = httpCode.find(info => info.code == errCode);
+addTitle(1);
+document.title = currentInfo.err[subStat].info_0;
+document.getElementById("wait_notify").remove();
+info0.innerHTML = currentInfo.err[subStat].info_0;
+info1.innerHTML = currentInfo.err[subStat].info_1;
+document.getElementById("service").innerHTML = "Powered by IIS v10.0.19041.1 / Windows 10 Pro 22H2";
