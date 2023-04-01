56d9cbce3468bb9dccb2399bf9a4d3283f070e93
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2023-04-02 05:37:38 +0700

    Removed dl cause I had a server. Updated main page

--- homepage.js
+++ homepage.js
@@ -4,6 +4,10 @@ const timerHour = document.getElementById("timer-hours");
 const timerMin = document.getElementById("timer-mins");
 const timerSec = document.getElementById("timer-secs");
 let now,t,days,hours,mins,secs = NaN;
+const params = new URLSearchParams(location.search);
+if (params.get("fromWWW") == "true") {createPopup("You are really dumb, aren't you ?<br>You should know that I hate <code>www.</code> subdomain.")}
+addTitle(1, false, true)
+document.getElementById("nav-software").href = `javascript:createPopup('<a style="color:lime" href="/dl/">/dl</a>&nbsp;(choomai.ddns.net only)<br><a style="color:lime" href="https://dl.choomai.tk/">dl.choomai.tk</a> or <a style="color:lime" href="https://dl.choomai.xyz">dl.choomai.xyz</a>')`;
 let timerFunc = setInterval(() => {
     now = new Date().getTime();
     t = endDate - now;
@@ -12,12 +16,12 @@ let timerFunc = setInterval(() => {
         hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
         secs = Math.floor((t % (1000 * 60)) / 1000);
-        timerDay.innerHTML = days + "<span class=\"smhd\">d</span>";
-        timerHour.innerHTML = hours + "<span class=\"smhd\">h</span>";
-        timerMin.innerHTML = mins + "<span class=\"smhd\">m</span>";
-        timerSec.innerHTML = secs + "<span class=\"smhd\">s</span>";
+        timerDay.innerHTML = days + "<span class='smhd'>d</span>";
+        timerHour.innerHTML = hours + "<span class='smhd'>h</span>";
+        timerMin.innerHTML = mins + "<span class='smhd'>m</span>";
+        timerSec.innerHTML = secs + "<span class='smhd'>s</span>";
     } else {
         document.getElementById("timer").innerHTML = "The countdown is over!";
         clearInterval(timerFunc);
     };
-}, 100);
+}, 1000);
